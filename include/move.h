e988c66b8834e933f070c97ddd5d7178b0b1ab21
Michał Śnieć <michal.sniec@gmail.com>
2019-04-12 17:55:59 +0200

    Add function overloads

--- include/move.h
+++ include/move.h
@@ -1,54 +1,8 @@
 #include <Arduino.h>
 #include <ISAMobile.h>
-// void SetPowerLevel(PowerSideEnum side, int level)
-// {
-//     level = constrain(level, -255, 255);
 
-//     if (side == PowerSideEnum::Right)
-//     {
-//         if (level > 0)
-//         {
-//             // do przodu
-//             digitalWrite(A_PHASE, 1);
-//             analogWrite(A_ENABLE, level);
-//         }
-//         else if (level < 0)
-//         {
-//             // do tyłu
-//             digitalWrite(A_PHASE, 0);
-//             analogWrite(A_ENABLE, -level);
-//         }
-//         else
-//         {
-//             // stop
-//             digitalWrite(A_PHASE, 0);
-//             analogWrite(A_ENABLE, 0);
-//         }
-//     }
-
-//     if (side == PowerSideEnum::Left)
-//     {
-//         if (level > 0)
-//         {
-//             // do przodu
-//             digitalWrite(B_PHASE, 1);
-//             analogWrite(B_ENABLE, level);
-//         }
-//         else if (level < 0)
-//         {
-//             // do tyłu
-//             digitalWrite(B_PHASE, 0);
-//             analogWrite(B_ENABLE, -level);
-//         }
-//         else
-//         {
-//             // stop
-//             digitalWrite(B_PHASE, 0);
-//             analogWrite(B_ENABLE, 0);
-//         }
-//     }
-// }
-void moveForward(PowerSideEnum side, int speed) {
+void moveForward(PowerSideEnum side, int speed)
+{
     if (side == PowerSideEnum::Right)
     {
         // do przodu
@@ -63,14 +17,16 @@ void moveForward(PowerSideEnum side, int speed) {
     }
 }
 
-void stopAll() {
+void stop()
+{
     digitalWrite(A_PHASE, 0);
     analogWrite(A_ENABLE, 0);
     digitalWrite(B_PHASE, 0);
     analogWrite(B_ENABLE, 0);
 }
 
-void moveBackwards(PowerSideEnum side, int speed) {
+void moveBackwards(PowerSideEnum side, int speed)
+{
     if (side == PowerSideEnum::Right)
     {
         // do przodu
@@ -85,8 +41,9 @@ void moveBackwards(PowerSideEnum side, int speed) {
     }
 }
 
-void stopSide(PowerSideEnum side) {
-        if (side == PowerSideEnum::Right)
+void stop(PowerSideEnum side)
+{
+    if (side == PowerSideEnum::Right)
     {
         // do przodu
         digitalWrite(A_PHASE, 0);
@@ -98,4 +55,16 @@ void stopSide(PowerSideEnum side) {
         digitalWrite(B_PHASE, 0);
         analogWrite(B_ENABLE, 0);
     }
+}
+
+void moveForward(int level)
+{
+    moveForward(PowerSideEnum::Left, level);
+    moveForward(PowerSideEnum::Right, level);
+}
+
+void moveBackwards(int level)
+{
+    moveBackwards(PowerSideEnum::Left, level);
+    moveBackwards(PowerSideEnum::Right, level);
 }
